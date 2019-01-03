882a62162303999ebf8c26ba945f5f2aebc6094c
OsetrovaD <liberty_20_07@mail.ru>
2019-01-03 19:48:19 +0300

    ArrayList was changed to LinkedList

--- src/test/java/com/osetrova/testutil/DumpUtil.java
+++ src/test/java/com/osetrova/testutil/DumpUtil.java
@@ -4,7 +4,7 @@ import com.osetrova.model.RobotDetail;
 import com.osetrova.util.RandomUtil;
 import lombok.experimental.UtilityClass;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @UtilityClass
@@ -13,7 +13,7 @@ public class DumpUtil {
     private static final int MAX_NUMBER = 20;
 
     public static List<RobotDetail> fillDump() {
-        List<RobotDetail> details = new ArrayList<>();
+        List<RobotDetail> details = new LinkedList<>();
         RobotDetail[] robotDetails = RobotDetail.values();
 
         for (int i = 0; i < MAX_NUMBER; i++) {
